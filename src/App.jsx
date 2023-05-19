43abb8a0556168dd5b159b5c6d78ccdcdce908c9
helmihidzir <helmihidzir@gmail.com>
2023-05-19 21:02:58 +0800

    Add holdDice

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,56 @@ import Die from "./components/Die.jsx"
 import { nanoid } from "nanoid"
 
 export default function App() {
-  const [dice, setDice] = React.useState(allNewDice())
+    const [dice, setDice] = React.useState(allNewDice())
 
-  function allNewDice() {
-    const randomNumbers = []
-
-    for (let i = 0; i < 10; i++) {
-      randomNumbers.push({
-          value: Math.ceil(Math.random() * 6),
-          isHeld: false,
-          id: nanoid()
-      })
+    function generateNewDie() {
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
     }
 
-    return randomNumbers
-  }
-  function rollDice() {
-    setDice(allNewDice())
-  }
+    function allNewDice() {
+        const randomNumbers = []
 
-  const diceElements = dice.map(die => <Die key={die.id} value={die.value} />)
+        for (let i = 0; i < 10; i++) {
+          randomNumbers.push(generateNewDie())
+        }
 
-  return (
-    <main>
-      <div className="dice-container">
-        {diceElements}
-      </div>
+        return randomNumbers
+    }
 
-      <button className="roll-dice" onClick={rollDice}>Roll</button>
-    </main>
-  )
+    function rollDice() {
+        setDice(prevDice =>
+            prevDice.map(die => {
+                return die.isHeld === false ?
+                    generateNewDie() :
+                    die
+            }))
+    }
+
+    function holdDice(id) {
+        setDice(prevDice =>
+            prevDice.map(die => {
+                return die.id === id ?
+                    { ...die, isHeld: !die.isHeld } :
+                    die
+            })
+        )
+    }
+
+    const diceElements = dice.map(die =>
+        <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />
+    )
+
+    return (
+        <main>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
+        </main>
+    )
 }
